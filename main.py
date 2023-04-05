db47bc4b99adcfb730b4b0ae0419d979b85b3eb6
Ali <poold189@gmail.com>
2023-04-05 21:19:56 +0500

    Removed annoying comments

--- main.py
+++ main.py
@@ -1,96 +1,111 @@
-#==========================Imports=========================#
+# =Imports
 import pygame as pg
 import os
 import time as tm
-#==========================Imports=========================#
 
-#===================Initializing================#
+# Imports
+
+# Initializing
 pg.init()
 pg.font.init()
 pg.mixer.init()
-#===================Initializing================#
 
-#=========================Main-Window=========================#
+
+# Main-Window
 WIDTH, HEIGHT = 900, 500
-BORDER = pg.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
-WIN = pg.display.set_mode((WIDTH, HEIGHT)) 
-pg.display.set_caption('Blyat')
-#=========================Main-Window=========================#                    
+BORDER = pg.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
+WIN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Blyat")
 
-#=========================Color=========================#
-WHITE = (255,255,255)
+
+# Color
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-YELLOW = (255,255,0)  
-#=========================Color=========================#
+YELLOW = (255, 255, 0)
 
-#=========================FPS-Limit=========================#
+
+# FPS-Limit
 FPS = 60
 PLAYING = True
-#=========================FPS-Limit=========================#
 
-#=========================HITREG-EVENTS=========================#
+
+# HITREG-EVENTS
 YELLOW_HIT = pg.USEREVENT + 1
 RED_HIT = pg.USEREVENT + 2
-#=========================SHIP=========================#
+# SHIP
 SHIP_WIDTH = 60
-SHIP_HEIGHT = 50  
+SHIP_HEIGHT = 50
 VEL = 6.76
-HEALTH_FONT = pg.font.SysFont('', 30)
-#=========================SHIP=========================#
+HEALTH_FONT = pg.font.SysFont("", 30)
 
-#---------------------BULLET----------------#  
+
+# BULLET
 BULLET_VEL = 15
-MAX_BULLETS = 3  
-#---------------------BULLET----------------# 
+MAX_BULLETS = 3
 
-#----------------------IMPORT-ASSETS--------------------# 
-YELLOW_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pg.transform.rotate(pg.transform.scale(YELLOW_SPACESHIP_IMAGE , (SHIP_WIDTH,SHIP_HEIGHT)), (270))
-RED_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_red.png')) 
-RED_SPACESHIP = pg.transform.rotate(pg.transform.scale(RED_SPACESHIP_IMAGE , (SHIP_WIDTH,SHIP_HEIGHT)), (90))
-BACKGROUND_IMAGE = pg.image.load(os.path.join('Assets', 'space.jpeg')) 
-BACKGROUND = pg.transform.scale(BACKGROUND_IMAGE,(WIDTH,HEIGHT))
-BULLET_SOUND = pg.mixer.Sound(os.path.join('Assets/Gun.mp3'))
-HIT_SOUND = pg.mixer.Sound(os.path.join('Assets', 'hit.mp3'))
-DEATH_SOUND = pg.mixer.Sound(os.path.join('Assets', 'death.mp3'))
-#----------------------IMPORT-ASSETS--------------------#        
-#=========================================================Draw-Window============================================#    
-def draw_window(red, yellow, red_bullets,yellow_bullets, red_health, yellow_health):
-    WIN.fill(WHITE)     
-    WIN.blit(BACKGROUND, (0,0))
-    pg.draw.rect(WIN,BLACK,BORDER)
-    
-    red_health_text = HEALTH_FONT.render('Health: ' + str(red_health), 1, RED)
-    yellow_health_text = HEALTH_FONT.render('Health: ' + str(yellow_health), 1, YELLOW)
+
+# IMPORT-ASSETS
+YELLOW_SPACESHIP_IMAGE = pg.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pg.transform.rotate(
+    pg.transform.scale(YELLOW_SPACESHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT)), (270)
+)
+RED_SPACESHIP_IMAGE = pg.image.load(os.path.join("Assets", "spaceship_red.png"))
+RED_SPACESHIP = pg.transform.rotate(
+    pg.transform.scale(RED_SPACESHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT)), (90)
+)
+BACKGROUND_IMAGE = pg.image.load(os.path.join("Assets", "space.jpeg"))
+BACKGROUND = pg.transform.scale(BACKGROUND_IMAGE, (WIDTH, HEIGHT))
+BULLET_SOUND = pg.mixer.Sound(os.path.join("Assets/Gun.mp3"))
+HIT_SOUND = pg.mixer.Sound(os.path.join("Assets", "hit.mp3"))
+DEATH_SOUND = pg.mixer.Sound(os.path.join("Assets", "death.mp3"))
+
+
+# Draw-Window
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.fill(WHITE)
+    WIN.blit(BACKGROUND, (0, 0))
+    pg.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, RED)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, YELLOW)
     WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
     WIN.blit(yellow_health_text, (10, 10))
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
-#--------------------Drawing-Bullets-----------------#
+    # Drawing-Bullets
     for bullet in red_bullets:
         pg.draw.rect(WIN, RED, bullet)
     for bullet in yellow_bullets:
         pg.draw.rect(WIN, YELLOW, bullet)
-#--------------------Drawing-Bullets-----------------#
     pg.display.update()
-#=============================Draw-Winner========================#
+
+
+# Draw Winner
 def draw_winner(text):
-    if text == 'YELLOW WINS':
+    if text == "YELLOW WINS":
         draw_text = HEALTH_FONT.render(text, 1, YELLOW)
-        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2,
-                         HEIGHT/2 - draw_text.get_width()/2))
-        pg.display.update() 
+        WIN.blit(
+            draw_text,
+            (
+                WIDTH / 2 - draw_text.get_width() / 2,
+                HEIGHT / 2 - draw_text.get_width() / 2,
+            ),
+        )
+        pg.display.update()
     else:
         draw_text = HEALTH_FONT.render(text, 1, RED)
-        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2,
-                         HEIGHT/2 - draw_text.get_width()/2))
+        WIN.blit(
+            draw_text,
+            (
+                WIDTH / 2 - draw_text.get_width() / 2,
+                HEIGHT / 2 - draw_text.get_width() / 2,
+            ),
+        )
         pg.display.update()
-        pg.time.delay(5000) 
-      
-#=============================Draw-Winner========================#   
-           
-#------------------------Player1-Handling------------------------#    
+        pg.time.delay(5000)
+
+
 def yellow_handle_movement(keys_pressed, yellow):
     if keys_pressed[pg.K_a] and yellow.x - VEL > 0:
         yellow.x -= VEL
@@ -111,10 +126,10 @@ def red_handle_movement(keys_pressed, red):
         red.y -= VEL
     if keys_pressed[pg.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:
         red.y += VEL
-#------------------------Player2-Handling--------------------------#
 
-#-------------------BULLET-HANDLER----------------------#
-def bullet_handler(yellow_bullets,red_bullets,red,yellow):
+
+# BULLET HANDLER
+def bullet_handler(yellow_bullets, red_bullets, red, yellow):
     for bullet in yellow_bullets:
         bullet.x += BULLET_VEL
         if red.colliderect(bullet):
@@ -123,7 +138,7 @@ def bullet_handler(yellow_bullets,red_bullets,red,yellow):
             yellow_bullets.remove(bullet)
         if bullet.x > WIDTH:
             yellow_bullets.remove(bullet)
-        
+
     for bullet in red_bullets:
         bullet.x -= BULLET_VEL
         if yellow.colliderect(bullet):
@@ -132,72 +147,76 @@ def bullet_handler(yellow_bullets,red_bullets,red,yellow):
             red_bullets.remove(bullet)
         if bullet.x < 0:
             red_bullets.remove(bullet)
- #-------------------BULLET-HANDLER----------------------#  
-          
-#--------------------------------------------Logic--------------------------------------------------#
+
+
+# Logic
 def main():
     playing = True
     red_health = 10
-    yellow_health = 10 
+    yellow_health = 10
     yellow_bullets = []
     red_bullets = []
-    red = pg.Rect(840, 240, SHIP_WIDTH,SHIP_HEIGHT)
-    yellow = pg.Rect(10, 240, SHIP_WIDTH,SHIP_HEIGHT)
+    red = pg.Rect(840, 240, SHIP_WIDTH, SHIP_HEIGHT)
+    yellow = pg.Rect(10, 240, SHIP_WIDTH, SHIP_HEIGHT)
 
-#---------------------CLOCK----------------------#
-    clock = pg.time.Clock() 
-#---------------------CLOCK----------------------#   
-    
+    # CLOCK
+    clock = pg.time.Clock()
+
     while playing:
-        clock.tick(FPS)  
-         
+        clock.tick(FPS)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 playing = False
-                exit()
-            
+                pg.quit()
+
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                    bullet = pg.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
+                    bullet = pg.Rect(
+                        yellow.x + yellow.width,
+                        yellow.y + yellow.height // 2 - 2,
+                        10,
+                        5,
+                    )
                     yellow_bullets.append(bullet)
                     BULLET_SOUND.play()
-                    
+
                 if event.key == pg.K_RCTRL and len(red_bullets) < MAX_BULLETS:
-                    bullet = pg.Rect(red.x - red.width/2, red.y + red.height//2 - 2, 10, 5)
+                    bullet = pg.Rect(
+                        red.x - red.width / 2, red.y + red.height // 2 - 2, 10, 5
+                    )
                     red_bullets.append(bullet)
                     BULLET_SOUND.play()
-                    
+
             if event.type == RED_HIT:
                 red_health -= 1
-            
+
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
-        
-        winner_text = ''
+
+        winner_text = ""
         if red_health <= 0:
-            winner_text = 'YELLOW WINS'
+            winner_text = "YELLOW WINS"
             DEATH_SOUND.play()
-            
+
         if yellow_health <= 0:
-            winner_text = 'RED WINS'
+            winner_text = "RED WINS"
             DEATH_SOUND.play()
-            
+
         if winner_text != "":
             draw_winner(winner_text)
-            break     
-        YELLOW_HIT,RED_HIT
+            break
+        YELLOW_HIT, RED_HIT
         keys_pressed = pg.key.get_pressed()
-#--------------------------------------------Logic--------------------------------------------------#
 
-
-#-----------------------------------fucntion-calls-------------------------------#  
-        yellow_handle_movement(keys_pressed, yellow)        
+        # function-calls
+        yellow_handle_movement(keys_pressed, yellow)
         red_handle_movement(keys_pressed, red)
-        draw_window(red,yellow,red_bullets,yellow_bullets, red_health, yellow_health)
-        bullet_handler(yellow_bullets,red_bullets,red,yellow) 
-    pg.time.delay(5000) 
-    main()
-#-----------------------------------fucntion-calls-------------------------------#
-if __name__ == '__main__':                  
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
+        bullet_handler(yellow_bullets, red_bullets, red, yellow)
+    pg.time.delay(5000)
     main()
 
+
+if __name__ == "__main__":
+    main()
