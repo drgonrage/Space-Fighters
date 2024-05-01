676cf74c9be877fa3e358aa8002291e3c331d3ec
Ali Hassan <poold189@gmail.com>
2024-05-02 01:43:40 +0500

    Updated Collisions

--- Source/main.py
+++ Source/main.py
@@ -9,9 +9,10 @@ pg.mixer.init()
 
 # Main Window
 WIDTH, HEIGHT = 900, 500
-BORDER = pg.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
+
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("SpaceFighters")
+BORDER = pg.Rect((WIDTH // 2), 0, 10, HEIGHT)
 
 # Color
 WHITE = (255, 255, 255)
@@ -28,7 +29,7 @@ RED_HIT = pg.USEREVENT + 2
 # SHIP
 SHIP_WIDTH = 60
 SHIP_HEIGHT = 50
-VEL = 6.76
+VEL = 8
 HEALTH_FONT = pg.font.SysFont("impact", 30)
 DEATH_FONT = pg.font.SysFont("impact", 100)
 PAUSE_FONT = pg.font.SysFont("cascadia", 140)
@@ -36,19 +37,21 @@ PAUSE_FONT = pg.font.SysFont("cascadia", 140)
 pause_text = PAUSE_FONT.render("PAUSED", True, BLUE)
 
 # BULLET
-BULLET_VEL = 15
+BULLET_VEL = 25
 MAX_BULLETS = 3
 
 
 # IMPORT ASSETS
-YELLOW_SPACESHIP_IMAGE = pg.image.load(os.path.join("Source/Assets", "spaceship_yellow.png"))
-YELLOW_SPACESHIP = pg.transform.rotate(
+YELLOW_SPACESHIP = pg.image.load(os.path.join("Source/Assets", "spaceship_yellow (Custom).png"))
+
+"""YELLOW_SPACESHIP = pg.transform.rotate(
     pg.transform.scale(YELLOW_SPACESHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT)), (270)
-)
-RED_SPACESHIP_IMAGE = pg.image.load(os.path.join("Source/Assets", "spaceship_red.png"))
-RED_SPACESHIP = pg.transform.rotate(
+)"""
+
+RED_SPACESHIP = pg.image.load(os.path.join("Source/Assets", "spaceship_red (Custom).png"))
+"""RED_SPACESHIP = pg.transform.rotate(
     pg.transform.scale(RED_SPACESHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT)), (90)
-)
+)"""
 
 BACKGROUND_IMAGE = pg.image.load(os.path.join("Source/Assets", "space.jpeg"))
 BACKGROUND = pg.transform.scale(BACKGROUND_IMAGE, (WIDTH, HEIGHT))
@@ -57,7 +60,6 @@ YELLOW_BULLET = pg.image.load(os.path.join("Source/Assets", "YellowBullets.png")
 RED_BULLET = pg.image.load(os.path.join("Source/Assets", "RedBullets.png"))
 HIT_SOUND = pg.mixer.Sound(os.path.join("Source/Assets", "Hit.mp3"))
 DEATH_SOUND = pg.mixer.Sound(os.path.join("Source/Assets", "death.mp3"))
-
 
 # Pause-Menu
 def pause_menu(paused):
@@ -120,28 +122,27 @@ def draw_winner(text):
             ),
         )
         pg.display.update()
-        pg.time.delay(2000)
 
 
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pg.K_a] and yellow.x - VEL > 0:
+    if keys_pressed[pg.K_a] and (yellow.x - VEL) > 0:
         yellow.x -= VEL
-    if keys_pressed[pg.K_d] and yellow.x + VEL + yellow.width < BORDER.x:
+    if keys_pressed[pg.K_d] and (yellow.x + VEL + yellow.width) < BORDER.x:
         yellow.x += VEL
-    if keys_pressed[pg.K_w] and yellow.y - VEL > 0:
+    if keys_pressed[pg.K_w] and (yellow.y - (VEL / 2)) > 0:
         yellow.y -= VEL
-    if keys_pressed[pg.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:
+    if keys_pressed[pg.K_s] and (yellow.y + VEL + yellow.height) < HEIGHT + 3.4:
         yellow.y += VEL
 
 
 def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pg.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width + 12:
+    if keys_pressed[pg.K_LEFT] and (red.x - VEL) > (BORDER.x + BORDER.width):
         red.x -= VEL
-    if keys_pressed[pg.K_RIGHT] and red.x + VEL + red.width < WIDTH + 10:
+    if keys_pressed[pg.K_RIGHT] and (red.x + VEL + VEL + red.width) < WIDTH + 10:
         red.x += VEL
-    if keys_pressed[pg.K_UP] and red.y - VEL > 0:
+    if keys_pressed[pg.K_UP] and (red.y - VEL) > -3:
         red.y -= VEL
-    if keys_pressed[pg.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:
+    if keys_pressed[pg.K_DOWN] and (red.y + VEL + red.height) < HEIGHT + 4.5:
         red.y += VEL
 
 
@@ -170,42 +171,41 @@ def bullet_handler(yellow_bullets, red_bullets, red, yellow):
 def main():
     FPS:int = 60
     PAUSED:bool = False
-    playing:bool = True
+    PLAYING:bool = True
     red_health:int = 10
     yellow_health:int = 10
     yellow_bullets:list = []
     red_bullets:list = []
     yellow_random_y = rd.randint(20, HEIGHT - SHIP_HEIGHT)
     red_random_y = rd.randint(20, HEIGHT - SHIP_HEIGHT)
-    red = pg.Rect(840, red_random_y, SHIP_WIDTH, SHIP_HEIGHT)
-    yellow = pg.Rect(10, yellow_random_y, SHIP_WIDTH, SHIP_HEIGHT)
-
+    red = pg.Rect(840, red_random_y, 50, SHIP_HEIGHT)
+    yellow = pg.Rect(10, yellow_random_y, 50, SHIP_HEIGHT)
     # CLOCK
     clock = pg.time.Clock()
 
-    while playing:
+    while PLAYING:
         clock.tick(FPS)
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                playing = False
+                PLAYING = False
                 pg.quit()
                 sys.exit()
 
             if event.type == pg.KEYDOWN:
                 if not PAUSED:
-                    if event.key == pg.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    if event.key == pg.K_SPACE and len(yellow_bullets) < MAX_BULLETS:
                         bullet = pg.Rect(
                             yellow.x + yellow.width,
-                            yellow.y + yellow.height // 2 - 2,
-                            10,
+                            yellow.y + yellow.height // 2 - 3,
+                            8,
                             5,
                         )
                         yellow_bullets.append(bullet)
                         BULLET_SOUND.play()
 
-                    if event.key == pg.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    if event.key == pg.K_j and len(red_bullets) < MAX_BULLETS:
                         bullet = pg.Rect(
-                            red.x - red.width / 2, red.y + red.height // 2 - 2, 10, 5
+                            red.x - red.width / 2, red.y + red.height // 2 - 3, 8, 5
                         )
                         red_bullets.append(bullet)
                         BULLET_SOUND.play()
@@ -218,13 +218,11 @@ def main():
                         PAUSED = False
                         pause_menu(PAUSED)
 
-
             if event.type == RED_HIT:
                 red_health -= 1
-
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
-
+                
         winner_text = ""
         if yellow_health <=0 and red_health <= 0:
             winner_text = 'DRAW'    
@@ -247,7 +245,6 @@ def main():
             yellow_handle_movement(keys_pressed, yellow)
             red_handle_movement(keys_pressed, red)
             bullet_handler(yellow_bullets, red_bullets, red, yellow)
-
     pg.time.delay(1000)
     main()
 
